d31f65110db7becc5d5993652d4e2a5133a78235
Bruno Martins <bruno.martins217@gmail.com>
2021-09-22 13:25:34 -0300

    a context api no react

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header 
         onOpenNewTransactionModal={ handleOpenNewTransactionModal }
       />
@@ -28,6 +29,6 @@ export function App() {
         onRequestClose={ handleCloseNewTransactionModal }
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
