9767f1f27c8851eb649712ea36addf9badb90117
Bruno Martins <bruno.martins217@gmail.com>
2021-09-22 14:58:28 -0300

    finalizando insercao

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,11 +10,20 @@ interface Transaction {
   createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category';
+
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
 
 export function TransactionProvider({children}: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -23,8 +32,12 @@ export function TransactionProvider({children}: TransactionsProviderProps) {
     api.get('transactions').then((response) => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction : TransactionInput) {
+    api.post('transactions', transaction)
+  }
+
   return(
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{transactions, createTransaction}}>
       {children}
     </TransactionsContext.Provider>
   )
